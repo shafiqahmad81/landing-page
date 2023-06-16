a72381abc536ee20f02f0b1184726622d9ccb902
shafiqahmad81 <shafiqahmad081@gmail.com>
2023-06-16 16:22:59 +0600

    update hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,19 +1,21 @@
 import React from 'react';
+import Header from '../components/Header';
 import { heroData } from '../data';
 
 const Hero = () => {
   const {title, subtitle, btnText, image} = heroData;
   return (
     <section className=' lg:h-[900px] py-12'>
+      <Header />
       <div className=' container mx-auto h-full relative'>
         <div className=' flex flex-col xl:flex-row items-center h-full md:py-24'>
           <div className=' text-center xl:text-left xl:absolute'>
-            <h1 className=' text-6xl xl:max-w-[700px] mb-6 lg:mb-12' data-aos='fade-down'data-aos-delay='400' >{title}</h1>
+            <h1 className='xl:max-w-[700px] mb-6 lg:mb-12' data-aos='fade-down'data-aos-delay='400' >{title}</h1>
             <p className=' text-lg xl:max-w-[380px] mb-6 lg:mb-12'data-aos='fade-down'data-aos-delay='500' >{subtitle}</p>
-            <button className='btn  mb-8 xl:mb-0'data-aos='fade-down'data-aos-delay='600' >{btnText}</button>
+            <button className='btn btn-primary  mb-8 xl:mb-0'data-aos='fade-down'data-aos-delay='700' >{btnText}</button>
           </div>
-          <div className=' xl:absolute xl:right-12 xl:bottom-16'data-aos='fade-up'data-aos-delay='700' >
-            <img className='w-[500px] h-[500px]' src= {image} alt=''/>
+          <div className='max-w-full w-[500px] h-[500px] xl:absolute xl:right-0 xl:bottom-16'data-aos='fade-up'data-aos-delay='700' >
+            <img className='' src= {image} alt=''/>
           </div>
         </div>
       </div>
