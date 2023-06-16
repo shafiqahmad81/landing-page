20cabc77cc13929d3d36a488881b15f091ca2dd8
shafiqahmad81 <shafiqahmad081@gmail.com>
2023-06-16 14:48:12 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Aos from "aos";
+import 'aos/dist/aos.css';
+import React from "react";
+import Hero from "./components/Hero";
+const App = () =>{
+  Aos.init({
+    duration: 1800,
+    offset: 0,
+  })
+  return <div className=" overflow-hidden">
+    <Hero />
+  </div>;
+};
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,16 @@
+import HandImage from '../src/assets/img/hand.png';
+import ShopingImage from '../src/assets/img/shoping.png';
+
+
+export const heroData = {
+  title: `Order Products Fester Easier`,
+  subtitle:'Order yout favorite foots at any time and we will deliver them right to where you are.',
+  btnText: 'Get Started',
+  image: ShopingImage,
+};
+
+export const aboutData = {
+  image: HandImage,
+  title: 'Find Out A Little More About us',
+  subtitle: 'we are a company dedicated to the distribution of products by delivry to your home or to the place where you are',
+};
